94f064e1b77d61ae02724e643de4ecf03952599a
RohithRenz <rohithrajgopu@gmail.com>
2024-11-18 10:41:56 +0530

    F M

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB Atlas
-mongoose.connect('your_mongodb_atlas_connection_string', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://pbfbenny:<db_password>@cluster0.wua9x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.log(error));
 
